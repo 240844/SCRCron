7a2d461766271212ba7736cb10a3a205d4a6d4c5
240844 <mekiroriginal@gmail.com>
2025-02-22 16:29:43 +0100

    Z

--- cron.cpp
+++ cron.cpp
@@ -9,6 +9,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <stdlib.h>
+#include <string.h>
 #include "logger.h"
 #include <vector>
 
@@ -32,6 +33,8 @@ int main(int argc, char* argv[]) {
     }
     else {
         connect_to_server(shared_data, argc, argv);
+        close(server_id);
+        munmap(shared_data, sizeof(data_t));
     }
     printf("Hello, World!\n");
     return 0;
@@ -140,7 +143,7 @@ int start_server(int server_id) {
     destroy_logger();
     mq_unlink(TASK_QUEUE);
     shm_unlink(SHM_SERVER);
-
+    munmap(shared_data, sizeof(data_t));
     return server_id;
 }
 
@@ -153,6 +156,7 @@ void list_query(query_t* query) {
     int queue = mq_open(query->queue_name, O_WRONLY, 0666, &attr);
     for (int i=0; i<qvec.size();i++) {
         query_t* q = qvec.at(i);
+        q->terminate = false;
         mq_send(queue, (char*) q, sizeof(query_t), 1);
     }
 
@@ -188,8 +192,11 @@ void fill_query(query_t* query) {
     timer_itimerspec->it_value.tv_nsec = 0;
     timer_itimerspec->it_interval.tv_sec = query->time_interval;
     timer_itimerspec->it_interval.tv_nsec = 0;
-
-    timer_settime(timer_id, query->is_absolute, timer_itimerspec, NULL);
+    int is_abs = 0;
+    if (query->is_absolute) {
+        is_abs = TIMER_ABSTIME;
+    }
+    timer_settime(timer_id, is_abs, timer_itimerspec, NULL);
     free(timer_itimerspec);
     query->timer_id = timer_id;
 
@@ -225,6 +232,10 @@ void timer(const union sigval query_sig) {
 
 //client functions
 void connect_to_server(data_t* shared_data, int argc, char* argv[]) {
+    if (argc <= 1) {
+        printf("Not enough arguments\n");
+        return;
+    }
     if (shared_data == NULL) {
         errno = EINVAL;
         printf("Couldn't open mapped shared memory");
@@ -235,5 +246,114 @@ void connect_to_server(data_t* shared_data, int argc, char* argv[]) {
         printf("Couldn't access server pid");
         return;
     }
+
+    struct mq_attr attr;
+    attr.mq_flags = 0;
+    attr.mq_curmsgs = 0;
+    attr.mq_maxmsg = 10;
+    attr.mq_msgsize = sizeof(query_t);
+    mqd_t queue_write = mq_open(TASK_QUEUE, O_WRONLY, 0666, &attr);
+
+    if (queue_write == -1) {
+        printf("Couldn't access queue");
+        return;
+    }
+
+    query_t query = {0};
+    if (strcmp(argv[1], "add") == 0) {
+        query.operation = QUERY_ADD;
+
+        if (strcmp(argv[2], "rel") == 0) {
+            if (argc >= 4) {
+                sscanf(argv[3], "%ld", &query.time_value);
+
+                int i = 4;
+                if (strcmp(argv[i], "-req") == 0) {
+                    sscanf(argv[i+1], "%ld", &query.time_interval);
+                    i += 2;
+                }
+                strcat(query.command, argv[i]);
+                i++;
+                for (int j=i;j<argc; j++) {
+                    if (j!=i) strcat(query.command_args, " ");
+                    strcat(query.command_args, argv[j]);
+                }
+                query.is_absolute = false;
+                mq_send(queue_write, (char*)&query, sizeof(query_t), 0);
+            }
+        }
+        else if (strcmp(argv[2], "abs") == 0) {
+            if (argc >= 5) {
+                struct tm abs_time;
+                sscanf(argv[3], "%d%*c%d%*c%d", &abs_time.tm_hour, &abs_time.tm_min, &abs_time.tm_sec);
+                sscanf(argv[3], "%d%*c%d%*c%d", &abs_time.tm_mday, &abs_time.tm_mon, &abs_time.tm_year);
+
+                abs_time.tm_mon -= 1;
+                abs_time.tm_year -= 1900;
+
+                int i = 5;
+                if (strcmp(argv[i], "-req") == 0) {
+                    sscanf(argv[i+1], "%ld", &query.time_interval);
+                    i += 2;
+                }
+                strcat(query.command, argv[i]);
+                i++;
+                for (int j=i;j<argc; j++) {
+                    if (j!=i) strcat(query.command_args, " ");
+                    strcat(query.command_args, argv[j]);
+                }
+
+                query.time_value = mktime(&abs_time);
+                query.is_absolute = true;
+
+                mq_send(queue_write, (char*)&query, sizeof(query_t), 0);
+            }
+        }
+    }
+    else if (strcmp(argv[1], "del") == 0){
+        query.operation = QUERY_DELETE;
+        sscanf(argv[2], "%ld", &query.id);
+        mq_send(queue_write, (char*)&query, sizeof(query_t), 0);
+    }
+    else if (strcmp(argv[1], "list") == 0) {
+        query.operation = QUERY_LIST;
+
+        sprintf(query.queue_name, "TASK_QUEUE_%d", getpid());
+
+        int queue_read = mq_open(query.queue_name, O_RDONLY | O_CREAT, 0666, &attr);
+
+        if (queue_read == -1) {
+            printf("failed to open queue");
+            mq_unlink(query.queue_name);
+            mq_close(queue_write);
+            return;
+        }
+
+        mq_send(queue_write, (char*)&query, sizeof(query_t), 1);
+        query_t* res;
+        res = (query_t*) calloc(1, sizeof(query_t));
+        if (res == NULL) {
+            printf("failed to allocate memory\n");
+            mq_close(queue_read);
+            mq_unlink(query.queue_name);
+            mq_close(queue_write);
+            return;
+        }
+        while (true) {
+            mq_receive(queue_read, (char*)res, sizeof(query_t), NULL);
+            if (res->terminate) break;
+            printf("Query id:%ld\n", res->id);
+            printf("command:%s\n", res->command);
+            printf("arg:%s\n", res->command);
+        }
+        free(res);
+        mq_close(queue_read);
+        mq_unlink(query.queue_name);
+    }
+    else if (strcmp(argv[1], "close") == 0) {
+        query.operation = QUERY_EXIT;
+        mq_send(queue_write, (char*)&query, sizeof(query_t), 0);
+    }
+    mq_close(queue_write);
 
 }
